4f1ad770921d5a38f2fd07ec144ba1a9c124988e
ajayyadav2181 <ajayyadav2181@gmail.com>
2026-04-17 10:46:37 +0530

    Python Project

--- .github/workflows/test_calculator.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from calculator import add,sub,mul,div
-
-def test_add():
-    assert add(2,3) == 5 # actual == expected (if match then test case will pass)
-
-def test_sub():
-    assert sub(4,3)==1
